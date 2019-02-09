1f017371baf2c513f0727648c21326119ddc601e
Jon Allured <jon.allured@gmail.com>
2019-02-09 14:39:56 -0600

    Comply with RuboCop

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
-# Define an application-wide content security policy
-# For further information see the following documentation
+# Define an application-wide content security policy For further information
+# see the following documentation
 # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy
 
 # Rails.application.config.content_security_policy do |policy|
@@ -17,7 +17,9 @@
 # end
 
 # If you are using UJS then enable automatic nonce generation
-# Rails.application.config.content_security_policy_nonce_generator = -> request { SecureRandom.base64(16) }
+# Rails.application.config
+#   .content_security_policy_nonce_generator =
+#     -> request { SecureRandom.base64(16) }
 
 # Report CSP violations to a specified URI
 # For further information see the following documentation:
