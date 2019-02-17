8591ba1f1be5985a3bf40d3c59243548c86e4e50
Jon Allured <jon.allured@gmail.com>
2019-02-17 14:52:54 -0600

    Setup Rails credentials to work with app config
    This is the way I think it should work.

---
+++ config/initializers/01_forty_config.rb
@@ -0,0 +1,23 @@
+module Forty
+  def self.config
+    @config ||= create_config
+  end
+
+  private_class_method def self.create_config
+    attributes = load_config
+    Struct.new("Config", *attributes.keys, keyword_init: true)
+    Struct::Config.new(attributes)
+  end
+
+  private_class_method def self.load_config
+    credentials = Rails.application.credentials
+    name = ENV['CREDS_GROUP'].to_sym
+    group = credentials[name]
+
+    raise 'CREDS_GROUP not found' unless group
+
+    {}
+  end
+end
+
+Forty.config
