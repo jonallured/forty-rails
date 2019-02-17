312e28a61f9c69f45c1a74394bf95dd4f28dc12a
Jon Allured <jon.allured@gmail.com>
2019-02-17 13:30:12 -0600

    Rename app
    I didn't really mean for the app to end up called FortyWeb so I'm just
    cleaning that up here.

--- config/application.rb
+++ config/application.rb
@@ -4,7 +4,7 @@ require 'rails/all'
 
 Bundler.require(*Rails.groups)
 
-module FortyWeb
+module Forty
   class Application < Rails::Application
     config.active_record.schema_format = :sql
     config.load_defaults 5.2
