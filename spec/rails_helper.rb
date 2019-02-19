39716960e664d47732cfa7c221b69e1f6147a8a7
Jon Allured <jon.allured@gmail.com>
2019-02-18 21:38:49 -0600

    Ensure browser specs don't hit the network

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,7 @@ require File.expand_path('../config/environment', __dir__)
 # Prevent database truncation if the environment is production
 abort('Rails is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
+require 'webmock/rspec'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 ActiveRecord::Migration.maintain_test_schema!
@@ -19,3 +20,5 @@ RSpec.configure do |config|
     driven_by :selenium_chrome_headless
   end
 end
+
+WebMock.disable_net_connect!(allow_localhost: true)
