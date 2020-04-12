1202049125c8d5a92190bd49f290a09ce0bd503e
Jon Allured <jon.allured@gmail.com>
2020-04-12 13:58:10 -0500

    Add date range format

---
+++ config/initializers/date_formats.rb
@@ -0,0 +1,14 @@
+Range::RANGE_FORMATS.merge!(
+  date_span: proc do |start, stop|
+    same_year = start.year == stop.year
+    same_month = start.month == stop.month
+
+    rhs_format = same_month ? '%d, %Y' : '%b %d, %Y'
+    rhs = stop.strftime(rhs_format)
+
+    lhs_format = same_year ? '%b %d' : '%b %d, %Y'
+    lhs = start.strftime(lhs_format)
+
+    [lhs, rhs].join('-')
+  end
+)
