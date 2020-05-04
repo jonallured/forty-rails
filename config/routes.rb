45b529f6ba8bcc895fe3f602291805b69e31169a
Jon Allured <jon.allured@gmail.com>
2020-05-03 21:38:48 -0500

    Add super simple account page

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   devise_scope :user do
     get 'sign-in', to: 'devise/sessions#new'
-    get 'sign-out', to: 'devise/sessions#destroy'
+    get 'sign-out', to: 'devise/sessions#destroy', as: :sign_out
     get 'sign-up', to: 'devise/registrations#new'
   end
 
@@ -16,6 +16,8 @@ Rails.application.routes.draw do
   get 'work_weeks/:target', to: 'work_weeks#show', as: :work_week
 
   patch 'work_days/:id', to: 'work_days#update'
+
+  get :account, to: 'account#show'
 
   root to: redirect('/today')
 end
