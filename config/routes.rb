f98706978baf6bfc7c20124970168ea9eb13cb94
Jon Allured <jon.allured@gmail.com>
2020-05-16 09:54:41 -0500

    Customize devise a bit more

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,26 @@
+path_names = {
+  confirmation: 'confirm',
+  password: 'password',
+  registration: 'registration',
+  sign_in: 'sign-in',
+  sign_out: 'sign-out',
+  sign_up: 'sign-up'
+}
+
+devise_options = {
+  path: '',
+  path_names: path_names,
+  sign_out_via: :get
+}
+
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, devise_options
 
   devise_scope :user do
-    get 'sign-in', to: 'devise/sessions#new'
+    get 'forgot', to: 'devise/passwords#new', as: :forgot
+    get 'sign-in', to: 'devise/sessions#new', as: :sign_in
     get 'sign-out', to: 'devise/sessions#destroy', as: :sign_out
-    get 'sign-up', to: 'devise/registrations#new'
+    get 'sign-up', to: 'devise/registrations#new', as: :sign_up
   end
 
   get :ping, to: 'ping#show'
