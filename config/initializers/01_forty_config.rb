fdcee3b9fbc80d4dd54e8a71ac2403ae651e1c05
Jon Allured <jon.allured@gmail.com>
2020-05-25 14:33:36 -0500

    Configure and setup email for the app

--- config/initializers/01_forty_config.rb
+++ config/initializers/01_forty_config.rb
@@ -9,7 +9,7 @@ module Forty
     Struct::Config.new(attributes)
   end
 
-  private_class_method def self.load_config
+  private_class_method def self.load_config # rubocop:disable Metrics/AbcSize, Metrics/MethodLength
     credentials = Rails.application.credentials
     name = ENV.fetch('CREDS_GROUP', 'invalid').to_sym
     group = credentials[name]
@@ -17,6 +17,13 @@ module Forty
     raise "CREDS_GROUP '#{name}' not found" unless group
 
     {
+      smtp_address: group.dig(:smtp, :address),
+      smtp_authentication: group.dig(:smtp, :authentication),
+      smtp_domain: group.dig(:smtp, :domain),
+      smtp_password: group.dig(:smtp, :password),
+      smtp_port: group.dig(:smtp, :port),
+      smtp_user_name: group.dig(:smtp, :user_name),
+
       stripe_plan_id: group.dig(:stripe, :plan_id),
       stripe_private_key: group.dig(:stripe, :private_key),
       stripe_public_key: group.dig(:stripe, :public_key)
