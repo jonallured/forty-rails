e90d65a16f1d9974e9d92374f547839f939590a5
Jon Allured <jon.allured@gmail.com>
2020-05-27 22:37:32 -0500

    Setup sidekiq web ui

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -6,3 +6,28 @@ if Rails.env.development?
   Sidekiq.configure_server { |config| config.redis = redis_config }
   Sidekiq.configure_client { |config| config.redis = redis_config }
 end
+
+# Protect against timing attacks:
+# - See https://codahale.com/a-lesson-in-timing-attacks/
+# - See https://thisdata.com/blog/timing-attacks-against-string-comparison/
+# - Use & (do not use &&) so that it doesn't short circuit.
+# - Use digests to stop length information leaking
+#   (see also ActiveSupport::SecurityUtils.variable_size_secure_compare)
+module SidekiqCreds
+  def self.checker
+    proc do |username, password|
+      expected_username = Forty.config.sidekiq_admin_username
+      expected_password = Forty.config.sidekiq_admin_password
+
+      compare(username, expected_username) &
+        compare(password, expected_password)
+    end
+  end
+
+  def self.compare(provided, expected)
+    hexed_provided = ::Digest::SHA256.hexdigest(provided)
+    hexed_expected = ::Digest::SHA256.hexdigest(expected)
+
+    ActiveSupport::SecurityUtils.secure_compare(hexed_provided, hexed_expected)
+  end
+end
