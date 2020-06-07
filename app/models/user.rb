76ee521ffc9d2ef2888da7c736ca81c5e84726fd
Jon Allured <jon.allured@gmail.com>
2020-06-07 16:01:51 -0500

    Add trackable to Users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
-  devise :database_authenticatable, :recoverable,
-         :registerable, :rememberable, :validatable
+  devise :database_authenticatable, :recoverable, :registerable, :rememberable, :trackable, :validatable
 
   has_many :subscriptions, dependent: :destroy
   has_many :work_days, dependent: :destroy
