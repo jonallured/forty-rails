25244e3f8800c59fd649a33bf352d307ed94968d
Jon Allured <jon.allured@gmail.com>
2020-11-16 15:19:00 -0600

    Add method to user for target eligibility

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,7 +1,19 @@
 FactoryBot.define do
-  factory :user do
+  factory :user, aliases: [:free_user] do
     confirmed_at { Time.zone.now }
     sequence(:email) { |n| "user#{n}@example.com" }
     password { 'sh' * 9 }
+
+    factory :active_user do
+      after(:create) do |user|
+        FactoryBot.create(:subscription, user: user)
+      end
+    end
+
+    factory :comped_user do
+      after(:create) do |user|
+        FactoryBot.create(:comped_subscription, user: user)
+      end
+    end
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,4 +12,57 @@ describe User do
     expect(user.valid?).to eq true
     expect(user.persisted?).to eq true
   end
+
+  describe '#can_view?' do
+    let(:active_user) { FactoryBot.create :active_user }
+    let(:comped_user) { FactoryBot.create :comped_user }
+    let(:free_user) { FactoryBot.create :free_user }
+
+    before { travel_to(Time.zone.local(2020, 1, 7)) }
+    after { travel_back }
+
+    context 'with this week' do
+      let(:year) { '2020' }
+      let(:number) { '02' }
+
+      it 'returns true for all users' do
+        expect(active_user.can_view?(year, number)).to eq true
+        expect(comped_user.can_view?(year, number)).to eq true
+        expect(free_user.can_view?(year, number)).to eq true
+      end
+    end
+
+    context 'with last week' do
+      let(:year) { '2020' }
+      let(:number) { '01' }
+
+      it 'returns true for all users' do
+        expect(active_user.can_view?(year, number)).to eq true
+        expect(comped_user.can_view?(year, number)).to eq true
+        expect(free_user.can_view?(year, number)).to eq true
+      end
+    end
+
+    context 'with next week' do
+      let(:year) { '2020' }
+      let(:number) { '03' }
+
+      it 'returns true for active and comped users but false for free users' do
+        expect(active_user.can_view?(year, number)).to eq true
+        expect(comped_user.can_view?(year, number)).to eq true
+        expect(free_user.can_view?(year, number)).to eq false
+      end
+    end
+
+    context 'with last year' do
+      let(:year) { '2019' }
+      let(:number) { '52' }
+
+      it 'returns true for active and comped users but false for free users' do
+        expect(active_user.can_view?(year, number)).to eq true
+        expect(comped_user.can_view?(year, number)).to eq true
+        expect(free_user.can_view?(year, number)).to eq false
+      end
+    end
+  end
 end
