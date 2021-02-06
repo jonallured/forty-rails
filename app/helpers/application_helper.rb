14a977d1f36b743983b576deda7d03c979de4af6
Jon Allured <jon.allured@gmail.com>
2021-02-06 12:00:21 -0600

    Add link to password blog post

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def password_post_link
+    post_url = 'https://www.fortyeven.com/blog/2021/02/06/nudging-the-world-towards-stronger-passwords.html'
+    link_to 'learn more', post_url, target: '_blank', rel: 'noopener'
+  end
 end
