488c3f62de590e7e0f510ee4199e18d6fbd472b8
Jon Allured <jon.allured@gmail.com>
2022-01-08 20:53:15 -0600

    Use Rails 7 defaults

--- config/application.rb
+++ config/application.rb
@@ -7,6 +7,6 @@ Bundler.require(*Rails.groups)
 module Forty
   class Application < Rails::Application
     config.active_record.schema_format = :sql
-    config.load_defaults 6.1
+    config.load_defaults 7.0
   end
 end
